71aa88461cee9f224f9c35115f487231eff8868e
harshvardhan singh <harshvardhansingh458@protonmail.com>
2022-12-07 13:58:33 +0530

    added axios api calls

--- frontend/src/apiCalls.js
+++ frontend/src/apiCalls.js
@@ -1,12 +1,108 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const loginCall = async (userCredential, dispatch) => {
-//     dispatch({ type: "LOGIN_START" });
-//     try {
-//         const res = await axios.post("http://localhost:5000/authentication/login", userCredential);
-//         console.log(res.data);
-//         dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-//     } catch (error) {
-//         dispatch({ type: "LOGIN_FAILURE", payload: error });
-//     }
-// }
+const baseUrl = "http://localhost:5000";
+const api = axios.create()
+
+// authentication api calls
+export const authenticationApiCalls = {
+    register: (signupDetails) => api.post(baseUrl + "/authentication/register", signupDetails),
+    login: (signinDetails) => api.post(baseUrl + "/authentication/login", signinDetails)
+}
+
+// users api calls
+export const usersApiCalls = {
+    update: (userDetails, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + "/users/update", userDetails);
+    },
+    updatePassword: (newPassword, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + "/users/updatePassword", newPassword);
+    },
+    delete: (accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.delete(baseUrl + "/users/delete");
+    },
+    getUser: (query) => {
+        return api.get(baseUrl + `/users/search?query=${query}`);
+    },
+    follow: (userId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + `/follow/${userId}`);
+    },
+    unFollow: (userId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + `/unfollow/${userId}`);
+    }
+}
+
+// post api calls
+export const postsApiCalls = {
+    create: (postDetails, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.post(baseUrl + "/posts/create", postDetails);
+    },
+    update: (postDetails, postId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + `/posts/update/${postId}`, postDetails);
+    },
+    delete: (postId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.delete(baseUrl + `/posts/delete/${postId}`);
+    },
+    like: (postId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + `/posts/like/${postId}`);
+    },
+    heart: (postId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.put(baseUrl + `/posts/heart/${postId}`);
+    },
+    getTimelinePosts: (accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.get(baseUrl + "/posts/timeline");
+    },
+    getAllPosts: (accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.get(baseUrl + "/posts/allPosts");
+    },
+    getPost: (postId) => api.get(baseUrl + `/posts/${postId}`)
+}
+
+// comments api calls
+export const commentsApiCalls = {
+    add: (postId, commentDetails, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.post(baseUrl + `/comments/add/${postId}`, commentDetails);
+    },
+    delete: (postId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.delete(baseUrl + `/comments/${postId}`);
+    },
+    getComments: (postId, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.get(baseUrl + `/comments/${postId}`);
+    },
+    reply: (commentId, replyDetails, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        return api.post(baseUrl + `/comments/reply/${commentId}`, replyDetails);
+    },
+    like: (commentId, type, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        if (type === "main") {
+            return api.put(baseUrl + `/comments/like/${commentId}`);
+        }
+        else if (type === "reply") {
+            return api.put(baseUrl + `/comments/reply/like/${commentId}`)
+        }
+    },
+    heart: (commentId, type, accessToken) => {
+        api.defaults.headers.common["authorization"] = "Bearer " + accessToken;
+        if (type === "main") {
+            return api.put(baseUrl + `/comments/heart/${commentId}`);
+        }
+        else if (type === "reply") {
+            return api.put(baseUrl + `/comments/reply/heart/${commentId}`);
+        }
+    }
+}
