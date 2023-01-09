3a70d97f50e6dcb56ff4cdd3172cc562c0540298
harshvardhan singh <harshvardhansingh458@protonmail.com>
2023-01-09 17:11:16 +0530

    added creating post functionality

--- frontend/src/components/Share/Share.jsx
+++ frontend/src/components/Share/Share.jsx
@@ -1,13 +1,56 @@
+import { useContext, useState } from "react";
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "firebase/storage"
+import { FacebookCloneContext } from "../../context/FacebookContext";
+import { postsApiRequests } from "../../services/apiCalls";
+import storage from "../../firebase";
 import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
 import "./Share.css";
 
 const Share = () => {
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const { user, setPosts } = useContext(FacebookCloneContext);
+    const [description, setDescription] = useState("");
+    const [file, setFile] = useState("");
+
+    const share = async () => {
+        const fileName = new Date().getTime() + file;
+        const storageRef = ref(storage, fileName);
+        uploadBytes(storageRef, file).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then(url => {
+                const postDetails = {
+                    description: description,
+                    media: url
+                };
+                const accessToken = localStorage.getItem("ACCESS_TOKEN");
+                postsApiRequests.create(postDetails, accessToken).then(response => {
+                    console.log("post uploaded: ", response);
+                    if (response.status === 201) {
+                        window.alert("Post Done");
+                    }
+                    else {
+                        window.alert("Something went wrong.");
+                    }
+                });
+
+            })
+        });
+    }
+
     return (
         <div className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <img src="/assets/person/1.jpeg" alt="" className="shareProfileImage" />
-                    <input placeholder="What's in your mind Harshvardhan?" className="shareInput" />
+                    <img src={user?.profilePictureUrl || PF + "person/noAvatar.png"} alt="" className="shareProfileImage" />
+                    <input
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                        placeholder={`What's in your mind ${user?.username}?`}
+                        className="shareInput"
+                    />
                 </div>
                 <hr className="shareHr" />
                 <div className="shareBottom">
@@ -15,6 +58,11 @@ const Share = () => {
                         <div className="shareOption">
                             <PermMedia htmlColor="tomato" className="shareIcon" />
                             <span className="shareOptionText">Photo or Video</span>
+                            <input
+                                type="file"
+                                value={file}
+                                onChange={(event) => setFile(event.target.files[0])}
+                            />
                         </div>
                         <div className="shareOption">
                             <Label htmlColor="blue" className="shareIcon" />
@@ -29,7 +77,7 @@ const Share = () => {
                             <span className="shareOptionText">Feelings</span>
                         </div>
                     </div>
-                    <button className="shareButton">Share</button>
+                    <button className="shareButton" onClick={share}>Share</button>
                 </div>
             </div>
         </div>
