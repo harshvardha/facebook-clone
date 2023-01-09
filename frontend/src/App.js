52294bf85c7d7dbcf05c1263fa7ce171698c715f
harshvardhan singh <harshvardhansingh458@protonmail.com>
2023-01-09 11:24:17 +0530

    route path fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import Register from "./pages/Register/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FacebookCloneProvider } from "./context/FacebookContext";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile/:username" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <FacebookCloneProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile/:username" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </FacebookCloneProvider>
   );
 }
 
